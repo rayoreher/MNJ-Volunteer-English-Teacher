2937f45fb95a3ee1a035a36a8930a8ffff9bc2d5
Ray Orellana <rayoreher@gmail.com>
2024-10-31 03:39:32 +0100

    fixed hero componenet

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,35 @@
-const Hero = () => {
-    return (
-		<div className="fh5co-hero">
-			<div className="fh5co-overlay"></div>
-			<div className="fh5co-cover text-center" data-stellar-background-ratio="0.5" 
-			style={{ 
-				backgroundImage: 'url(/MNJ-Volunteer-English-Teacher/images/cover_bg_1.jpg)',
-				backgroundSize: 'cover',
-				backgroundPosition: 'center' }}>
-				<div className="desc animate-box">
-					<h2><strong>MNJ Volunteer English Teacher</strong></h2>
-					<span><a className="btn btn-primary btn-lg" href="#">Volunteer Now</a></span>
-				</div>
-			</div>
+import Link from "next/link";
 
-		</div>
-    );
-  };
-  
-  export default Hero;
+const Hero = () => {
+  return (
+    <section
+      className="relative text-white min-h-[calc(100vh-9rem)] flex items-center justify-center"
+      style={{
+        backgroundImage:
+          'url("/MNJ-Volunteer-English-Teacher/images/cover_bg_1.jpg")',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="absolute inset-0 bg-black opacity-60"></div>
+
+      <div className="relative z-10 container mx-auto px-4 text-center">
+        <h1 className="text-4xl md:text-5xl font-bold mb-6">
+          Inspiring Young Minds Together
+        </h1>
+        <p className="text-xl mb-8">
+          Empower children in Mae Najang with education and fun activities. Join
+          us in shaping brighter futures.
+        </p>
+        <Link
+          href="/volunteer"
+          className="bg-lime-600 text-white px-7 py-4 rounded-md font-semibold text-lg hover:bg-lime-500 transition duration-300"
+        >
+          Volunteer Now
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
