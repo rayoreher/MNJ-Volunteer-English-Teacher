6f2b05d80196cc9a2b5477db450723fe88713a0e
Ray Orellana <rayoreher@gmail.com>
2024-11-01 18:09:19 +0100

    bug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,11 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import Layout from "@/components/Layout";
-import Head from "next/head";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Layout from '@/components/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <meta
-          name="description"
-          content="Website for the MNJ Volunteer English Teacher project"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/MNJ-Volunteer-English-Teacher/favicon.ico" />
-      </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <Head>
+        <meta name="description" content="Website for the MNJ Volunteer English Teacher project" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/MNJ-Volunteer-English-Teacher/favicon.ico" />
+      </Head>
       <body>
           <Main />
           <NextScript />
