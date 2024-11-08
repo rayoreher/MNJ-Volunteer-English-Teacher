3bd237a1d6ac135c2c4957dabc78892e2046dd3a
Ray Orellana <rayoreher@gmail.com>
2024-11-08 19:32:29 +0100

    fix: solve bug where favicon not found

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -33,9 +33,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-      <meta name="description" content="Website for the MNJ Volunteer English Teacher project" />
+        <meta name="description" content="Website for the MNJ Volunteer English Teacher project" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/MNJ-Volunteer-English-Teacher/favicon.ico" />
+        <link rel="icon" href="favicon.ico" />
       </Head>
       <Layout>
         <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,6 @@ const Home = () => {
     <>
       <Head>
         <title>MNJ Volunteer English Teacher</title>
-        <meta name="description" content="Website for the MNJ Volunteer English Teacher project" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/MNJ-Volunteer-English-Teacher/favicon.ico" />
       </Head>
       <Main />
     </>
