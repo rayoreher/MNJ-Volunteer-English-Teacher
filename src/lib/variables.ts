7e25acd04b84b3601a93f1813abb1baa3dd5cf0c
Ray Orellana <rayoreher@gmail.com>
2024-11-09 19:44:40 +0100

    feat: update functions url

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -1,5 +1,5 @@
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const hCaptchaSiteKey = process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY;
-const supabaseFunctionsUrl = process.env.NEXT_PUBLIC_SUPABASE_FUNCTIONS_URL;
+const supabaseFunctionsUrl = process.env.NEXT_PUBLIC_SUPABASE_URL + '/functions/v1/api';
 export  { supabaseUrl, supabaseAnonKey, hCaptchaSiteKey, supabaseFunctionsUrl };
--- supabase/functions/api/volunteer/index.ts
+++ supabase/functions/api/volunteer/index.ts
@@ -5,7 +5,7 @@ import { form } from './form/index.ts';
 const volunteer = new Hono().basePath("/volunteer");
 
 volunteer.use(cors({
-    origin: "http://localhost:3000"
+    origin: ["http://localhost:3000", "http://mnj-volunteer-project.rayoreher.com/", "https://mnj-volunteer-project.rayoreher.com/"],
 }));
 
 volunteer.route("", form);
