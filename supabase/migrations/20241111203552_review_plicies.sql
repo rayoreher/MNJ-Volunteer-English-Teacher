0f1f835663de4d493ec31b74b42af55b7170c097
Ray Orellana <rayoreher@gmail.com>
2024-11-11 21:42:20 +0100

    solve sql error_2

--- supabase/migrations/20241111203552_review_plicies.sql
+++ supabase/migrations/20241111203552_review_plicies.sql
@@ -1,3 +1,7 @@
+drop policy "everyone can create" on "public"."reviews";
+
+drop policy "only logged in users can edit" on "public"."reviews";
+
 drop policy "everyone can create" on "public"."volunteers";
 
 alter table "public"."reviews" drop column "content";
@@ -6,11 +10,24 @@ alter table "public"."reviews" add column "comment" text not null default ''::te
 
 alter table "public"."reviews" alter column "fullname" set default ''::text;
 
-alter table "public"."reviews" alter column "id" set default gen_random_uuid();
-
 alter table "public"."reviews" alter column "id" drop identity;
 
-alter table "public"."reviews" alter column "id" set data type uuid using "id"::uuid;
+create policy "only logged in users can create"
+on "public"."reviews"
+as permissive
+for insert
+to authenticated
+with check (true);
+
+
+create policy "only logged in users can update"
+on "public"."reviews"
+as permissive
+for update
+to authenticated
+using (true)
+with check (true);
+
 
 create policy "only logged in users can create"
 on "public"."volunteers"
